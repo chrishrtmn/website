200f33bf5116ae03aaf636375864a357ce0201ac
Chris Hartman <chrishartman@me.com>
2021-11-16 13:21:51 -0500

    Adding hotjar tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import PlausibleProvider from 'next-plausible'
+import { hotjar } from 'react-hotjar'
+import { useEffect } from 'react'
 
 import '../styles/base.css'
 import '../styles/components.css'
@@ -6,6 +8,9 @@ import '../styles/custom.css'
 import '../styles/utilities.css'
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    hotjar.initialize(1941197, 6)
+  }, [])
   return (
     <PlausibleProvider domain="chrishrtmn.com">
       <Component {...pageProps} />
