ce810fa623a685312d54bf123084d0de37d0a198
Chris Hartman <chrishartman@me.com>
2021-11-17 17:11:12 -0500

    Removing Hotjar tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,4 @@
 import PlausibleProvider from 'next-plausible'
-import { hotjar } from 'react-hotjar'
-import { useEffect } from 'react'
 
 import '../styles/base.css'
 import '../styles/components.css'
@@ -8,11 +6,8 @@ import '../styles/custom.css'
 import '../styles/utilities.css'
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    hotjar.initialize(1941197, 6)
-  }, [])
   return (
-    <PlausibleProvider domain="chrishrtmn.com">
+    <PlausibleProvider domain='chrishrtmn.com'>
       <Component {...pageProps} />
     </PlausibleProvider>
   )
